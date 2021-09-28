267f16997e972005df5d048eb7d3c09e7949af01
ilyes7 <fitouriilyes7@gmail.com>
2021-09-28 22:32:54 +0200

    seance2

---
+++ src/app/Models/User.ts
@@ -0,0 +1,13 @@
+export class user{
+
+idUser: number;
+firstName: string;
+lastName: string;
+birthDate: string;
+email: string;
+password: string;
+profession: string;
+accountCategory: string;
+picture: string;
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { ContentComponent } from './content/content.component';
+import { ListUserComponent } from './list-user/list-user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    ContentComponent,
+    ListUserComponent,
   ],
   imports: [
     BrowserModule,
